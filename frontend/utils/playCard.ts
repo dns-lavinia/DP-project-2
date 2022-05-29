bf8f5ddab7a67c0042dccfcf75b5bdd08faba89e
Eliza Mărțuică <32893642+iuliameliza@users.noreply.github.com>
2022-05-29 17:04:48 +0300

    Update playCard.ts

--- frontend/utils/playCard.ts
+++ frontend/utils/playCard.ts
@@ -15,33 +15,116 @@ export function PlayCard(gameRound: IGameRound, chosenCard: ICard){
     }
     
     if( gameRound.gameRules.cheating == false ){    //Cheating NOT allowed
-        if( chosenCard.suit == gameRound.hand.dealtCards[0].suit || chosenCard.suit == gameRound.tromf ){ //same color
-            if( gameRound.gameRules.bigger == true ){ //BIGGER
-                for( var c in gameRound.hand.dealtCards ){
-                    if( gameRound.hand.dealtCards[c].value > chosenCard.value ) // If smaller value return false
-                        return false;
-                }
+        if( gameRound.hand.currentPlayer == 'player1' ){
+            if( noColorCardInHand(gameRound.deck.player1, gameRound.tromf, gameRound.hand.dealtCards[0], chosenCard) == false )
+                return false;
+            if( gameRound.gameRules.bigger == true ){    // Iber
+                if( iber(gameRound.hand.dealtCards, chosenCard, gameRound.tromf) == false )
+                    return false;
+            }
+        }else if( gameRound.hand.currentPlayer == 'player2' ){
+            if( noColorCardInHand(gameRound.deck.player2, gameRound.tromf, gameRound.hand.dealtCards[0], chosenCard) == false )
+                return false;
+            if( gameRound.gameRules.bigger == true ){    // Iber
+                if( iber(gameRound.hand.dealtCards, chosenCard, gameRound.tromf) == false )
+                    return false;
+            }
+        }else if( gameRound.hand.currentPlayer == 'player3' ){
+            if( noColorCardInHand(gameRound.deck.player3, gameRound.tromf, gameRound.hand.dealtCards[0], chosenCard) == false )
+                return false;
+            if( gameRound.gameRules.bigger == true ){    // Iber
+                if( iber(gameRound.hand.dealtCards, chosenCard, gameRound.tromf) == false )
+                    return false;
+            }
+        }else if( gameRound.hand.currentPlayer == 'player4' ){
+            if( noColorCardInHand(gameRound.deck.player4, gameRound.tromf, gameRound.hand.dealtCards[0], chosenCard) == false )
+                return false;
+            if( gameRound.gameRules.bigger == true ){    // Iber
+                if( iber(gameRound.hand.dealtCards, chosenCard, gameRound.tromf) == false )
+                    return false;
             }
         }
     }else{  // Cheating ALLOWED
-        if( chosenCard.suit == gameRound.hand.dealtCards[0].suit || chosenCard.suit == gameRound.tromf ){
-            if( gameRound.gameRules.bigger == true ){
-                for( var c in gameRound.hand.dealtCards ){
-                    if( gameRound.hand.dealtCards[c].value > chosenCard.value )
-                        if( gameRound.hand.currentPlayer == "player1" || gameRound.hand.currentPlayer == "player3" ) // mark team for cheating
-                            gameRound.team1Cheated= true;
-                        else
-                            gameRound.team2Cheated= true;
-                }
-            }
-        }else{
-            if( gameRound.hand.currentPlayer == "player1" || gameRound.hand.currentPlayer == "player3" ) // mark team for cheating
+        if( gameRound.hand.currentPlayer == 'player1' ){
+            if( noColorCardInHand(gameRound.deck.player1, gameRound.tromf, gameRound.hand.dealtCards[0], chosenCard) == false )
                 gameRound.team1Cheated= true;
-            else
+            if( gameRound.gameRules.bigger == true ){    // Iber
+                if( iber(gameRound.hand.dealtCards, chosenCard, gameRound.tromf) == false )
+                    gameRound.team1Cheated= true;
+            }
+        }else if( gameRound.hand.currentPlayer == 'player2' ){
+            if( noColorCardInHand(gameRound.deck.player2, gameRound.tromf, gameRound.hand.dealtCards[0], chosenCard) == false )
                 gameRound.team2Cheated= true;
+            if( gameRound.gameRules.bigger == true ){    // Iber
+                if( iber(gameRound.hand.dealtCards, chosenCard, gameRound.tromf) == false )
+                    gameRound.team2Cheated= true;
+            }
+        }else if( gameRound.hand.currentPlayer == 'player3' ){
+            if( noColorCardInHand(gameRound.deck.player3, gameRound.tromf, gameRound.hand.dealtCards[0], chosenCard) == false )
+                gameRound.team1Cheated= true;
+            if( gameRound.gameRules.bigger == true ){    // Iber
+                if( iber(gameRound.hand.dealtCards, chosenCard, gameRound.tromf) == false )
+                    gameRound.team1Cheated= true;
+            }
+        }else if( gameRound.hand.currentPlayer == 'player4' ){
+            if( noColorCardInHand(gameRound.deck.player4, gameRound.tromf, gameRound.hand.dealtCards[0], chosenCard) == false )
+                gameRound.team2Cheated= true;
+            if( gameRound.gameRules.bigger == true ){    // Iber
+                if( iber(gameRound.hand.dealtCards, chosenCard, gameRound.tromf) == false )
+                    gameRound.team2Cheated= true;
+            }
         }
     }
     
+    return true;
+}
+
+function noColorCardInHand(hand : ICard[], tromf : string, firstCard : ICard, chosenCard : ICard){
+    let color= false;
+    let tr= false;
+
+    for(let i in hand){     //checking if there is a card in hand with the requested color or tromf
+        if( hand[i].suit == firstCard.suit )
+            color= true;
+        if( hand[i].suit == tromf )
+            tr= true;
+    }
+
+    if( color == true ){    //if a color exists
+        if( chosenCard.suit == firstCard.suit ) // if chosen card is a color
+            return true;
+    }else if( tr == true ){ //if color doesnt exist but there is tromf
+        if( chosenCard.suit == tromf )  //if chosen card is tromf
+            return true;
+    }
+
+    return false;
+}
+
+function iber(dealtCards : ICard[], chosenCard: ICard, tromf : string){
+    let firstCardTromf= false;
+    let tromfDown= false;
+    let biggestValueDown= 0;
+
+    if( dealtCards[0].suit == tromf )
+        firstCardTromf= true;
+
+    for(let i=0 ; i<dealtCards.length ; i++){
+        if( i != 0 && dealtCards[i].suit == tromf )
+            tromfDown= true;
+        if( dealtCards[i].value > biggestValueDown )
+            biggestValueDown= dealtCards[i].value;
+    }
+
+    if( firstCardTromf == false ){
+        if( tromfDown == false )
+            if(chosenCard.value < biggestValueDown)
+                return false;
+    }else{
+        if(chosenCard.value < biggestValueDown )
+            return false;
+    }
+
     return true;
 }
 
