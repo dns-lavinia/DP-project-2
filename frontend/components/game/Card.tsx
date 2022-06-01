144ca506833946c88603e270bc7eb44a80d61b3a
Andreea-Lavinia DINIS <dns.lavinia@gmail.com>
2022-06-01 14:58:16 +0300

    added cards overlay

--- frontend/components/game/Card.tsx
+++ frontend/components/game/Card.tsx
@@ -1,17 +1,20 @@
 import type { ICard } from "types/game";
 
+
 interface CardProps extends ICard {}
 
 export default function Card({ suit, value }: CardProps) {
+    var card_name: string = suit.concat(value.toString());
+
     return (
         <div className="bg-dark-1 rounded-3xl cursor-pointer p-4 shadow-purple-300 shadow-lg transition-transform
             hover:-translate-y-20"
         >
-            <div className="w-56 h-96 rounded-xl border border-purple-300 flex flex-col divide-y divide-purple-300 justify-center overflow-hidden">
-                <img src="/card_XP.png" title="card"/>
-                {/* <img src="/card_XP.png" className="rotate-180"/> */}
-                <p>{suit}</p>
-                <p>{value}</p>
+            <div className="w-56 h-96 rounded-xl border border-purple-300 flex flex-col divide-y divide-purple-300 justify-center overflow-hidden">                
+                <img src={`/${card_name}.png`} title="card"/>
+                <img src={`/${card_name}.png`} className="rotate-180"/>
+                {/* <p>{suit}</p> */}
+                {/* <p>{value}</p> */}
             </div>
         </div>
     )
